f31c9780b4b3b4829bbf7e20a9e50c5ba3eac131
kezunhao <kezunhao@yeah.net>
2019-12-30 16:04:43 +0800

    inti

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -2,6 +2,12 @@
 use Swoole\Http\Server;
 
 $http = new Server("0.0.0.0", 8801);
+$http->set(
+    [
+        'enable_static_handle' => true,
+        'document_root' => __DIR__ . "../../html"
+    ]
+);
 $http->on('request', function ($request, $response) {
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
